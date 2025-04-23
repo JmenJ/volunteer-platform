085889788b85e0c45f2fd15acf686ee8fcf91b8e
LigHan <52926514+LigHan@users.noreply.github.com>
2025-04-23 13:40:40 +0300

    Add files via upload

--- test1/assets/js/login.js
+++ test1/assets/js/login.js
@@ -1,20 +1,22 @@
 document.getElementById('loginForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-  
-    const role = document.getElementById('userRole').value;
-    const login = document.getElementById('login').value;
-    const password = document.getElementById('password').value;
-  
-    if (!role) {
-      alert('Пожалуйста, выберите роль');
-      return;
-    }
-  
-    console.log(`Попытка входа:
-  Роль: ${role === 'volunteer' ? 'Волонтёр' : 'Партнёр'}
-  Логин: ${login}
-  Пароль: ${'*'.repeat(password.length)}`);
-  
-    alert(`Вход как ${role === 'volunteer' ? 'Волонтёр' : 'Партнёр'} выполнен`);
-  });
-  
+  e.preventDefault();
+
+  const role = document.getElementById('userRole').value;
+  const login = document.getElementById('login').value;
+  const password = document.getElementById('password').value;
+
+  if (!role) {
+    alert('Пожалуйста, выберите роль');
+    return;
+  }
+
+  console.log(`Попытка входа:
+Роль: ${role === 'volunteer' ? 'Волонтёр' : 'Партнёр'}
+Логин: ${login}
+Пароль: ${'*'.repeat(password.length)}`);
+
+  alert(`Вход как ${role === 'volunteer' ? 'Волонтёр' : 'Партнёр'} выполнен`);
+
+  // Перенаправление
+  window.location.href = "main.html";
+});
