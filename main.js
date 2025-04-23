01ea97756a7d59a673bd1f29c56c7198b6650850
egor20sp <slushatelnik@yandex.ru>
2025-04-23 15:29:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,11 +5,16 @@ function ObrabZapr(request, response){ //info / otvet
     if (request.method == "GET"){
         console.log(request.url);
         if (request.url == "/"){
-            const file = "./test1/html/main.html";
+            file = "./test1/html/main.html";
+        }
+        else if(request.url.substring(0, 8) === "/assets/"){
+            file = "./test1" + request.url;
         }
         else{
-            const file = "./test1"+request.url;
+            file = "./test1/html"+request.url;
         }
+        file = file.replaceAll("%20", " ");
+        console.log(file);
         response.setHeader("Content-Type", "text/html; charset=utf-8;");
         fs.stat(file, (err, stats) => {
             if(stats && stats.isFile() && !err){
@@ -33,4 +38,3 @@ function startServer(){                //&
 server = http.createServer(ObrabZapr);
 
 server.listen(3000, "127.0.0.1", startServer);
-
