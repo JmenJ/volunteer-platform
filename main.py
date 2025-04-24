3fc813f13c4b369c48b4cb4cf07290e4e528ce96
egor20sp <slushatelnik@yandex.ru>
2025-04-24 17:04:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, request
-import os, random, time
+from flask import Flask, request, make_response
+import os, random, time, sys
 from flask_mail import Mail, Message
 import csv
+
+print("\nLine: 6\n", file=sys.stderr)
 
 time_prost = 30 #minute
 
@@ -13,11 +15,16 @@ db_registrat = {} #{ip : [password, tip, [...]], ...}
 
 def update_time(ip):
     global db_ip
+    print("\nLine: 18\n", db_ip, file=sys.stderr)
     while len(db_ip)>0 and db_ip[0][1] + time_prost*60*10e+9 < time.time_ns():
+        print("\nLine: 20\n", len(db_ip), file=sys.stderr)
         db_ip.pop(0)
-    for i in db_ip:
-        if i[0] == ip:
-            i[1] = time.time_ns()
+    for i in range(len(db_ip)):
+        print("\nLine: 23\n", db_ip, file=sys.stderr)
+        if db_ip[i][0] == ip:
+            print("\nLine: 25\n", ip, file=sys.stderr)
+            db_ip[i][1] = time.time_ns()
+            print("\nLine: 27\n", db_ip[i], file=sys.stderr)
             return True
     return False
 
@@ -56,7 +63,7 @@ dataCSV.pop(0)
 
 import sqlite3
 
-db_connection = sqlite3.connect("./db_vol_part.db")
+db_connection = sqlite3.connect("./db_vol_part.db", check_same_thread=False)
 db_cursor = db_connection.cursor()
 
 #таблица Профилей Волонтёров
@@ -101,7 +108,7 @@ def editPartner(name_id, value_id, nameparam, value):
 
 def getByVolunteers(inn, param):
     db_cursor.execute('SELECT ? FROM Volunteers WHERE inn = ?', (param, inn))
-    return cursor.fetchall()
+    return db_cursor.fetchall()
 def getByPartner(name_id, value_id, param):
     db_cursor.execute('SELECT ? FROM Partner WHERE ? = ?', (param, name_id, value_id))
     return db_cursor.fetchall()
@@ -114,13 +121,27 @@ def getPartner(param, name_id):
 
 
 def obrabotka(ip, dir):
-    fl_name = "./test1/html"+dir
-    fl_name.replace("%20", " ")
-    if os.path.isfile(fl_name) and ".." not in fl_name:
-        data = open(fl_name, "r").read()
-        return True, data
-    else:
-        return False, ""
+    print("\nLine: 124\n", file=sys.stderr)
+    if (db_registrat[ip][1] == "Volunteers"):
+        print("\nLine: 126\n", file=sys.stderr)
+        if dir == "/main_vol.html":
+            print("\nLine: 128\n", file=sys.stderr)
+            fl_name = "./test1/html"+dir
+            print("\nLine: 130\n", file=sys.stderr)
+            return True, open(fl_name, "r").read()
+        else:
+            print("\nLine: 133\n", file=sys.stderr)
+            return False, ""
+    elif db_registrat[ip][1] == "Partner":
+        print("\nLine: 136\n", file=sys.stderr)
+        if dir == "/main_par.html":
+            print("\nLine: 138\n", file=sys.stderr)
+            fl_name = "./test1/html"+dir
+            print("\nLine: 140\n", file=sys.stderr)
+            return True, open(fl_name, "r").read()
+        else:
+            print("\nLine: 143\n", file=sys.stderr)
+            return False, ""
 
 
 
@@ -136,130 +157,190 @@ def main1():
 #     send_code_to("slushatelnik@yandex.ru")
 
 @app.route("/<dir>")
-#----------------------
 def prov(dir):
-    if ("/"+dir == "/login.html?prov"):
+    print("\nLine: 161\n", file=sys.stderr)
+    global db_ip
+    print("\nLine: 163\n", file=sys.stderr)
+    if ("/"+dir == "/login.html_prov"):
+        print("\nLine: 165\n", file=sys.stderr)
         tip = request.headers.get("MyType")
         login = request.headers.get("MyLogin")
         password = request.headers.get("MyPassword")
         ip = request.remote_addr
-        
+        print("\nLine: 170\n", file=sys.stderr)
+        response = make_response("")
+        response.headers["Content-Type"] = "text/plain"
         if not (isinstance(tip, str) and isinstance(login, str) and isinstance(password, str)):
+            print("\nLine: 174\n", file=sys.stderr)
             response.headers["MyState"] = "False"
             return response
         
-        response = make_response("")
-        response.headers["Content-Type"] = "text/plain"
-        
         for i in range(len(db_ip)):
+            print("\nLine: 179\n", file=sys.stderr)
             if db_ip[i][0] == ip:
+                print("\nLine: 181\n", file=sys.stderr)
                 db_ip.pop(i)
                 break
         
         if tip == "Partner":
+            print("\nLine: 186\n", file=sys.stderr)
             rez = getByPartner("login", login, "password")
+            print("\nLine: 188\n", file=sys.stderr)
             if len(rez) == 0:
+                print("\nLine: 190\n", file=sys.stderr)
                 response.headers["MyState"] = "False"
                 return response
             elif password == rez[0][0]:
+                print("\nLine: 194\n", file=sys.stderr)
                 response.headers["MyState"] = "True"
                 db_ip.append([ip, time.time_ns(), [login, tip]])
+                print("\nLine: 197\n", file=sys.stderr)
                 return response
             else:
                 response.headers["MyState"] = "False"
+                print("\nLine: 201\n", file=sys.stderr)
                 return response
         elif tip == "Volunteers":
-            rez = getByPartner(login, "password")
+            print("\nLine: 204\n", file=sys.stderr)
+            rez = getByVolunteers(login, "password")
+            print("\nLine: 206\n", file=sys.stderr)
             if len(rez) == 0:
+                print("\nLine: 208\n", file=sys.stderr)
                 response.headers["MyState"] = "False"
                 return response
-            elif password == rez[0][0]:
+            elif str(password) == rez[0][0]:
+                print("\nLine: 212\n", file=sys.stderr)
                 response.headers["MyState"] = "True"
                 db_ip.append([ip, time.time_ns(), [login, tip]])
+                print("\nLine: 215\n", file=sys.stderr)
                 return response
             else:
+                print("\nLine: 218\n", file=sys.stderr)
                 response.headers["MyState"] = "False"
                 return response
         else:
+            print("\nLine: 222\n", file=sys.stderr)
             response.headers["MyState"] = "False"
             return response
-    elif ("/"+dir == "/reg.html?data"):
+    elif ("/"+dir == "/reg.html_data"):
+        print("\nLine: 226\n", file=sys.stderr)
         tip = request.headers.get("MyType")
         ip = request.remote_addr
+        print("\nLine: 229\n", file=sys.stderr)
         if tip == "Volunteers":
+            print("\nLine: 231\n", file=sys.stderr)
             inn = request.headers.get("MyINN")
             password = request.headers.get("MyPassword")
+            print("\nLine: 234\n", file=sys.stderr)
             if not (isinstance(inn, str) and isinstance(password, str)):
                 response.headers["MyState"] = "False"
+                print("\nLine: 237\n", file=sys.stderr)
                 return response
+            print("\nLine: 239\n", file=sys.stderr)
             st = 0
-            for i in len(range(dataCSV)):
+            for i in range(len(dataCSV)):
+                print("\nLine: 242\n", file=sys.stderr)
                 if inn == dataCSV[i][1]:
+                    print("\nLine: 244\n", file=sys.stderr)
                     st = 1
                     break
+            print("\nLine: 247\n", file=sys.stderr)
             if st == 0:
+                print("\nLine: 249\n", file=sys.stderr)
                 response.headers["MyState"] = "False"
                 return response
             response = make_response("")
             response.headers["Content-Type"] = "text/plain"
             response.headers["MyState"] = "True"
+            print("\nLine: 255\n", file=sys.stderr)
             db_registrat[ip] = [password, tip, [inn]]
+            print("\nLine: 257\n", file=sys.stderr)
             return response
         elif tip == "Partner":
+            print("\nLine: 260\n", file=sys.stderr)
             login = request.headers.get("MyLogin")
             email = request.headers.get("MyEmail")
             password = request.headers.get("MyPassword")
+            print("\nLine: 264\n", file=sys.stderr)
             if not (isinstance(login, str) and isinstance(password, str) and isinstance(email, str)):
                 response.headers["MyState"] = "False"
+                print("\nLine: 267\n", file=sys.stderr)
                 return response
             response = make_response("")
             response.headers["Content-Type"] = "text/plain"
             response.headers["MyState"] = "True"
             db_registrat[ip] = [password, tip, [login, email]]
+            print("\nLine: 273\n", file=sys.stderr)
             return response
         else:
             response = make_response("")
             response.headers["Content-Type"] = "text/plain"
             response.headers["MyState"] = "False"
             return response
-    elif ("/"+dir == "/reg.html?code"):
+    elif ("/"+dir == "/reg.html_code"):
+        print("\nLine: 281\n", file=sys.stderr)
         tip = request.headers.get("MyType")
         ip = request.remote_addr
-        
+        print("\nLine: 284\n", file=sys.stderr)
         response = make_response("")
         response.headers["Content-Type"] = "text/plain"
-        
+        print("\nLine: 287\n", file=sys.stderr)
         if ip in db_registrat:
+            print("\nLine: 289\n", file=sys.stderr)
             if tip == db_registrat[ip][1]:
+                print("\nLine: 291\n", file=sys.stderr)
                 code = request.headers.get("MyCode")
+                print("\nLine: 293\n", file=sys.stderr)
                 if isinstance(code, str):
+                    print("\nLine: 295\n", file=sys.stderr)
                     response.headers["MyState"] = "True"
                     #proverka koda
+                    print("\nLine: 298\n", file=sys.stderr)
                     if tip == "Volunteers":
+                        print("\nLine: 300\n", file=sys.stderr)
                         addVolunteers(db_registrat[ip][0], 0, db_registrat[ip][2][0])
+                        db_ip.append([ip, time.time_ns(), [db_registrat[ip][2][0], tip]])
+                        response.headers["MyState"] = "True"
+                        print("\nLine: 304\n", file=sys.stderr)
                         return response
                     elif tip == "Partner":
+                        print("\nLine: 307\n", file=sys.stderr)
                         addPartner(db_registrat[ip][2][0], db_registrat[ip][2][1], db_registrat[ip][0])
+                        db_ip.append([ip, time.time_ns(), [db_registrat[ip][2][0], tip]])
+                        response.headers["MyState"] = "True"
+                        print("\nLine: 311\n", file=sys.stderr)
                         return response
+        print("\nLine: 313\n", file=sys.stderr)
         response.headers["MyState"] = "False"
         return response
                     
     elif ("/"+dir in sp_svob_fail):
+        print("\nLine: \n", file=sys.stderr)
         return open("./test1/html/"+dir, "r").read()
     else:
+        print("\nLine: 321\n", file=sys.stderr)
         ip = request.remote_addr
         st = update_time(ip)
+        print("\nLine: 324\n", file=sys.stderr)
         if st:
+            print("\nLine: 326\n", file=sys.stderr)
             for i in range(len(db_ip)):
+                print("\nLine: 328\n", file=sys.stderr)
                 if db_ip[i][0] == ip:
+                    print("\nLine: 330\n", file=sys.stderr)
                     db_ip[i][1] = time.time_ns()
+                    print("\nLine: 332\n", file=sys.stderr)
                     break
             st, data = obrabotka(ip, "/"+dir)
+            print("\nLine: 335\n", file=sys.stderr)
             if st:
+                print("\nLine: 337\n", file=sys.stderr)
                 return data;
             else:
+                print("\nLine: 340\n", file=sys.stderr)
                 return open("./test1/html/login.html", "r").read()
         else:
+            print("\nLine: 343\n", file=sys.stderr)
             return open("./test1/html/login.html", "r").read()
 #----------------------
 
